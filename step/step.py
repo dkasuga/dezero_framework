51a28c3c944e2daae08b5001c79820dba6bf8f55
dkasuga <benjamin.at.mac@gmail.com>
2021-05-15 08:23:15 +0900

    step7 add auto backward

--- step/step.py
+++ step/step.py
@@ -4,27 +4,66 @@ import numpy as np
 class Variable:
     def __init__(self, data):
         self.data = data
+        self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        f = self.creator  # Get a function
+        if f is not None:
+            x = f.input  # Get the function's input
+            x.grad = f.backward(self.grad)  # call the function's backward
+            x.backward()
+
 
 class Function:
     def __call__(self, input):
         x = input.data
-        y = self.forward(x) 
+        y = self.forward(x)
         output = Variable(y)
+        output.set_creator(self)  # Set parent(function)
+        self.input = input
+        self.output = output  # Set output
         return output
-    
+
     def forward(self, x):
         raise NotImplementedError()
 
+    def backward(self, gy):
+        raise NotImplementedError()
+
+
 class Square(Function):
     def forward(self, x):
-        return x ** 2
+        y = x ** 2
+        return y
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
 
 class Exp(Function):
     def forward(self, x):
         return np.exp(x)
 
-x = Variable(np.array(10))
-f = Square()
-y = f(x)
-print(type(y))
-print(y.data)
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+
+
+A = Square()
+B = Exp()
+C = Square()
+
+x = Variable(np.array(0.5))
+a = A(x)
+b = B(a)
+y = C(b)
+
+y.grad = np.array(1.0)
+y.backward()
